50c03409ad81556cce9272a95519a7c061fa32f9
Ryan Bertram <rpbertram@users.noreply.github.com>
2016-11-01 13:47:53 -0500

    Completed golden 7

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -15,8 +15,25 @@ def create_row
   p.caption = params[ :the_caption]
   p.source = params[ :the_source]
   p.save
-
   # render("create_row.html.erb")
+  redirect_to("http://localhost:3000/photos")
+end
+
+def destroy
+  Photo.find_by({ :id => params[:id] }).destroy
+  redirect_to("http://localhost:3000/photos")
+end
+
+def edit_form
+  @photo = Photo.find_by({ :id => params[:id] })
+  render("edit_form.html.erb")
+end
+
+def update_photo
+  p = Photo.find_by({ :id => params[:id] })
+  p.caption = params[ :the_caption]
+  p.source = params[ :the_source]
+  p.save
   redirect_to("http://localhost:3000/photos")
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
-  # Rountes to CREATE photos
+  # Routes to CREATE photos
   get("/photos/new", { :controller => "photos", :action => "new_form"})
   get("/create_photo", { :controller => "photos", :action => "create_row"})
+
+  # Routes to DELETE photos
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy"})
+
+  # Routes to EDIT photos
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_photo"})
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
