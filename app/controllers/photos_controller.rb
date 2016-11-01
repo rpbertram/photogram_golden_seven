b19657486d663def29de462add0c4604890687ea
Ryan Bertram <rpbertram@users.noreply.github.com>
2016-11-01 12:55:54 -0500

    Got thru video

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,21 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+def show
+  @photo = Photo.find_by({ :id => params[:id] })
+end
+
+def new_form
+end
+
+def create_row
+  p = Photo.new
+  p.caption = params[ :the_caption]
+  p.source = params[ :the_source]
+  p.save
+
+  # render("create_row.html.erb")
+  redirect_to("http://localhost:3000/photos")
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  # Rountes to CREATE photos
+  get("/photos/new", { :controller => "photos", :action => "new_form"})
+  get("/create_photo", { :controller => "photos", :action => "create_row"})
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
